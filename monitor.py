b48d9646bc24c6d4681154ca358b8ab061f26109
githejie <www.617311008@qq.com>
2017-11-08 02:24:22 +0800

    itchat

--- monitor.py
+++ monitor.py
@@ -10,12 +10,16 @@ import datetime
 import imutils
 import time
 import cv2
+import itchat
 
+
+#登录微信
+itchat.auto_login(hotReload=True)
 
 # 初始化摄像头并且获取一个指向原始数据的引用
 camera = PiCamera()
 camera.resolution = tuple([640, 480])
-camera.framerate = 16
+camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=tuple([640, 480]))
  
 # 等待摄像头模块启动, 随后初始化平均帧, 最后
@@ -46,9 +50,8 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
         rawCapture.truncate(0)
         continue
  
-    # accumulate the weighted average between the current frame and
-    # previous frames, then compute the difference between the current
-    # frame and running average
+    # 在当前帧和先前帧之间累积加权平均值，
+    # 然后计算当前帧与运行平均值之间的差值。
     cv2.accumulateWeighted(gray, avg, 0.5)
     frameDelta = cv2.absdiff(gray, cv2.convertScaleAbs(avg))
 
@@ -79,15 +82,48 @@ for f in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True
     cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
         0.35, (0, 0, 255), 1)
 
+    # 检测该房间是否被“占领”
+    if text == "Occupied":
+        # 判断上传时间间隔是否已经达到
+        if (timestamp - lastUploaded).seconds >= 3.0:
+            # 运动检测计数器递增
+            motionCounter += 1
+
+            # 判断包含连续运动的帧数是否已经足够多
+            if motionCounter >= 8:
+                # 判断微信是否被使用
+                if 1 == 1:
+                    # 将图像写入临时文件
+                    t = TempImage()
+                    cv2.imwrite(t.path, frame)
+
+                    # 将图像上传至微信并删除临时图片
+                    print "[UPLOAD] {}".format(ts)
+                    path = "{base_path}/{timestamp}.jpg".format(
+                        base_path=conf["dropbox_base_path"], timestamp=ts)
+                    itchat.send_image(path,'filehelper')
+                    t.cleanup()
+
+                # 更新最近一次上传的时间戳并且重置运动计数器
+                lastUploaded = timestamp
+                motionCounter = 0
+
+    # 否则, 该房间没有“被占领”
+    else:
+        motionCounter = 0
+
     # 判断安保视频是否需要显示在屏幕上
     if 1 == 1:
         # 显示安视频
         cv2.imshow("Security Feed", frame)
         key = cv2.waitKey(1) & 0xFF
- 
+
         # 如果q被按下，跳出循环
         if key == ord("q"):
+            # 退出微信
+            itchat.logout()
             break
- 
+
+
     # 清理数据流为下一帧做准备
     rawCapture.truncate(0)
